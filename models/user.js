ade164cf8774360977100ee92a72ccfee19ee73d
andysml <andy922200@gmail.com>
2019-10-14 21:50:41 +0800

    Fix relations in User model

--- models/user.js
+++ models/user.js
@@ -13,10 +13,17 @@ module.exports = (sequelize, DataTypes) => {
     // associations can be defined here
     User.hasMany(models.ShippingAddress)
     User.hasMany(models.Order)
+    User.hasMany(models.Comment)
+    User.hasMany(models.Reply)
     User.belongsToMany(models.Coupon, {
       through: models.UserCoupon,
       foreignKey: 'UserId',
       as: 'OwnCoupons'
+    })
+    User.belongsToMany(models.Product, {
+      through: models.WishItem,
+      foreignKey: 'UserId',
+      as: 'WishProducts'
     })
   };
   return User;
