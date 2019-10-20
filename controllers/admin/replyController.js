4c2fdf7206ed34bffd6204fca110c30d3493760d
Brian.L <brian9500885@gmail.com>
2019-10-20 12:16:05 +0800

    camelCase

--- controllers/admin/replyController.js
+++ controllers/admin/replyController.js
@@ -1,11 +1,11 @@
 const replyController = {
-  createreply: (req, res) => {
+  createReply: (req, res) => {
     // 新增回應
   },
-  putreply: (req, res) => {
+  putReply: (req, res) => {
     // 修改回應
   },
-  deletereply: (req, res) => {
+  deleteReply: (req, res) => {
     // 刪除回應
   }
 }
