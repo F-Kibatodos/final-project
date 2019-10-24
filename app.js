25ac67a0549f847fddc85bae304562eca460be7e
Brian.L <brian9500885@gmail.com>
2019-10-24 15:16:49 +0800

    add signin and signup

--- app.js
+++ app.js
@@ -1,10 +1,27 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
+const bodyParser = require('body-parser')
+const flash = require('connect-flash')
+const session = require('express-session')
+const passport = require('./config/passport')
+
 const app = express()
 
+app.use(bodyParser.urlencoded({ extended: true }))
 app.use(express.static('public'))
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
+app.use(session({ secret: 'secret', resave: false, saveUninitialized: false }))
+app.use(flash())
+app.use(passport.initialize())
+app.use(passport.session())
 
-require('./routes')(app)
+app.use((req, res, next) => {
+  res.locals.success_messages = req.flash('success_messages')
+  res.locals.error_messages = req.flash('error_messages')
+  res.locals.user = req.user
+  next()
+})
+
+require('./routes')(app, passport)
 app.listen(3000)
