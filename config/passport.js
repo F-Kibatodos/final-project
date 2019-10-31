f0203883b2091de862de44e101aa344d6998998b
andysml <andy922200@gmail.com>
2019-10-31 16:36:59 +0800

    Facebook Login

--- config/passport.js
+++ config/passport.js
@@ -1,5 +1,6 @@
 const passport = require('passport')
 const LocalStrategy = require('passport-local')
+const FacebookStrategy = require('passport-facebook')
 const bcrypt = require('bcrypt-nodejs')
 const db = require('../models')
 const User = db.User
@@ -33,6 +34,44 @@ passport.use(
     }
   )
 )
+
+passport.use(
+  new FacebookStrategy(
+    {
+      clientID: process.env.FACEBOOK_ID,
+      clientSecret: process.env.FACEBOOK_SECRET,
+      callbackURL: process.env.FACEBOOK_CALLBACK,
+      profileFields: ['email', 'displayName']
+    },
+    (accessToken, refreshToken, profile, done) => {
+      User.findOne({ where: { email: profile._json.email } })
+        .then(user => {
+          if (!user) {
+            var randomPassword = Math.random().toString(36).slice(-8)
+            bcrypt.genSalt(10, (err, salt) => {
+              bcrypt.hash(randomPassword, salt, (err, hash) => {
+                var newUser = new User({
+                  name: profile._json.name,
+                  email: profile._json.email,
+                  password: hash,
+                  role: 0
+                })
+                newUser
+                  .save()
+                  .then(user => {
+                    return done(null, user)
+                  })
+                  .catch(err => {
+                    console.log(err)
+                  })
+              })
+            })
+          } else {
+            return done(null, user)
+          }
+        })
+    }
+  ))
 
 // serialize and deserialize user
 passport.serializeUser((user, cb) => {
