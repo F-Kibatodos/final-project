0de69f435770fcc0cc76a892e62e7c228b003bf4
Brian.L <brian9500885@gmail.com>
2019-11-02 22:38:35 +0800

    adjust code

--- public/javascripts/product.js
+++ public/javascripts/product.js
@@ -1,37 +1,37 @@
-window.onload = function() {
-  var star = document.getElementById('star')
-  var star_li = star.getElementsByTagName('li')
-  var star_word = document.getElementById('star_word')
-  var result = document.getElementById('result')
-  var i = 0
-  var j = 0
-  var len = star_li.length
-  var word = ['很差', '差', '一般', '好', '很好']
-  for (i = 0; i < len; i++) {
-    star_li[i].index = i
-
-    star_li[i].onclick = function() {
-      for (i = 0; i <= 4; i++) {
-        if (i <= this.index) star_li[i].className = 'act'
-        else star_li[i].classList.remove('act')
-      }
-    }
-  }
-}
+var star = document.getElementById('star')
+var star_li = star.getElementsByTagName('li')
+var star_word = document.getElementById('star_word')
+var result = document.getElementById('result')
+var i = 0
+var j = 0
+var len = star_li.length
 const buy = document.querySelector('.buy')
 const toCart = document.querySelector('.to-cart')
 const buyNow = document.querySelector('.buy-now')
+const editComments = document.getElementsByClassName('edit-comment')
+const cancelUpdates = document.getElementsByClassName('cancel-update')
+let originalText
+const editRelpies = document.getElementsByClassName('edit-reply')
+const cancelUpdateReplies = document.getElementsByClassName(
+  'cancel-update-reply'
+)
+let originalReplyText
+
+for (i = 0; i < len; i++) {
+  star_li[i].index = i
+
+  star_li[i].onclick = function() {
+    for (i = 0; i <= 4; i++) {
+      if (i <= this.index) star_li[i].className = 'act'
+      else star_li[i].classList.remove('act')
+    }
+  }
+}
 
 buy.addEventListener('mouseover', function(e) {
   if (e.target === toCart) buy.setAttribute('action', '/cart')
   if (e.target === buyNow) buy.setAttribute('action', '/buynow')
 })
-
-const customComments = document.getElementsByClassName('custom-comment')
-const editComments = document.getElementsByClassName('edit-comment')
-const commentWrappers = document.getElementsByClassName('comment-wrapper')
-const cancelUpdates = document.getElementsByClassName('cancel-update')
-let originalText
 
 for (let editComment of editComments) {
   editComment.addEventListener('click', e => {
@@ -56,11 +56,6 @@ for (let cancelUpdate of cancelUpdates) {
   })
 }
 
-const editRelpies = document.getElementsByClassName('edit-reply')
-const cancelUpdateReplies = document.getElementsByClassName(
-  'cancel-update-reply'
-)
-let originalReplyText
 for (let editReply of editRelpies) {
   editReply.addEventListener('click', e => {
     originalReplyText =
