247b14cd240d2f0bf01e5705d11080047465a77f
andysml <andy922200@gmail.com>
2019-11-12 03:36:52 +0800

    admin/orders put function & layout

--- controllers/admin/orderController.js
+++ controllers/admin/orderController.js
@@ -1,6 +1,8 @@
 const db = require('../../models')
 const User = db.User
+const Product = db.Product
 const Order = db.Order
+const OrderItem = db.OrderItem
 const Sequelize = require('sequelize')
 const Op = Sequelize.Op
 
@@ -16,14 +18,38 @@ const orderController = {
         return res.render('admin/orders', { orders: data })
       })
   },
-  createOrder: (req, res) => {
-    // 新增訂單
+  editOrder: (req, res) => {
+    Order.findByPk(req.params.id).then(order => {
+      OrderItem.findAll({
+        where: { OrderId: order.id }, include: [Product], attributes: ['id', 'ice', 'sugar']
+      }).then(items => {
+        res.render('admin/edit-order', { items: items, order: order })
+      })
+    })
   },
   putOrder: (req, res) => {
-    // 修改訂單
+    Order.findByPk(req.params.id)
+      .then(order => {
+        OrderItem.findAll({
+          where: { OrderId: req.params.id }, attributes: ['id', 'ice', 'sugar']
+        }).then(items => {
+          for (var i = 0; i < items.length; i++) {
+            console.log(order.id, items[i].id)
+            let temp1 = 'ice' + items[i].id
+            let temp2 = 'sugar' + items[i].id
+            OrderItem.findOne({ where: { 'id': items[i].id } }).then(item => {
+              item.update({
+                ice: req.body[temp1],
+                sugar: req.body[temp2]
+              })
+            })
+          }
+          req.flash('success_messages', `訂單 ${order.sn} 已更新`)
+          res.redirect('/admin/orders')
+        })
+      })
   },
   deleteOrder: (req, res) => {
-    // 刪除訂單
     return Order.findByPk(req.params.id)
       .then(order => {
         order.destroy()
